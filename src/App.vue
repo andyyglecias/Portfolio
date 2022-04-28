9b5e67eceabe50ac9b68166d5803a5cbfeb317bb
Andy <andyyglecias@gmail.com>
2022-04-28 15:25:38 -0500

    took out sections

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,95 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <nav class="sections-nav-container">
+      <ul id="sections-nav" class="nav sections-nav sections-nav-animated">
+        <li class="sections-nav-item">
+          <a
+            href="#section-01"
+            class="nav-link sections-nav-link goto-section active"
+          >
+            <span class="sections-nav-counter">01</span>
+            Top
+          </a>
+        </li>
+        <li class="sections-nav-item">
+          <a href="#section-02" class="nav-link sections-nav-link goto-section">
+            <span class="sections-nav-counter">02</span>
+            About me
+          </a>
+        </li>
+        <li class="sections-nav-item">
+          <a href="#section-03" class="nav-link sections-nav-link goto-section">
+            <span class="sections-nav-counter">03</span>
+            My services
+          </a>
+        </li>
+        <li class="sections-nav-item">
+          <a href="#section-04" class="nav-link sections-nav-link goto-section">
+            <span class="sections-nav-counter">04</span>
+            Skills
+          </a>
+        </li>
+        <li class="sections-nav-item">
+          <a href="#section-05" class="nav-link sections-nav-link goto-section">
+            <span class="sections-nav-counter">05</span>
+            Facts
+          </a>
+        </li>
+        <li class="sections-nav-item">
+          <a href="#section-06" class="nav-link sections-nav-link goto-section">
+            <span class="sections-nav-counter">06</span>
+            Experiences
+          </a>
+        </li>
+        <li class="sections-nav-item">
+          <a href="#section-07" class="nav-link sections-nav-link goto-section">
+            <span class="sections-nav-counter">07</span>
+            References
+          </a>
+        </li>
+        <li class="sections-nav-item">
+          <a href="#section-08" class="nav-link sections-nav-link goto-section">
+            <span class="sections-nav-counter">08</span>
+            Projects
+          </a>
+        </li>
+        <li class="sections-nav-item">
+          <a href="#section-09" class="nav-link sections-nav-link goto-section">
+            <span class="sections-nav-counter">09</span>
+            Testimonials
+          </a>
+        </li>
+        <li class="sections-nav-item">
+          <a href="#section-10" class="nav-link sections-nav-link goto-section">
+            <span class="sections-nav-counter">10</span>
+            Contact
+          </a>
+        </li>
+        <li class="sections-nav-item">
+          <div class="sections-nav-info">
+            <a href="mailto:john@wilson.com">john@wilson.com</a><br />
+            <a href="tel:+420652887351">+420 652 887 351</a>
+          </div>
+        </li>
+      </ul>
     </nav>
-    <router-view/>
+    <header class="header">
+      <div
+        class="container-fluid-limited d-flex align-items-center justify-content-between"
+      >
+        <a href="index.html" class="header-brand">
+          <img src="assets/img/logo.svg" alt="Your logo" />
+        </a>
+        <button class="sections-nav-toggler" aria-label="Toggle navigation">
+          <span class="sections-nav-toggler-bar"></span>
+          <span class="sections-nav-toggler-bar"></span>
+          <span class="sections-nav-toggler-bar"></span>
+          <span class="sections-nav-toggler-bar"></span>
+        </button>
+      </div>
+    </header>
+    <router-view />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,242 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <main class="content">
+      <div class="container-fluid-limited">
+        <div class="row">
+          <div class="col col-xl-9">
+            <section
+              id="section-01"
+              class="section section-sub-header animation interaction-in"
+            >
+              <div class="section-body">
+                <div class="jumbotron jumbotron-fluid pt-6 pt-lg-8 pb-0 mb-0">
+                  <img
+                    src="https://via.placeholder.com/160"
+                    class="jumbotron-img animation-translate animation-item-1"
+                    alt="Avatar"
+                  />
+                  <h1
+                    class="display-1 display-animated display-animated-in animation-translate animation-item-2"
+                  >
+                    Andy<br />Yglecias
+                  </h1>
+                  <p class="lead animation-translate animation-item-3">
+                    FREELANCE WEBDEVELOPER
+                  </p>
+                </div>
+              </div>
+              <div class="section-footer animation-translate animation-item-4">
+                <a class="section-next goto-section" href="#section-02">
+                  <span class="section-next-counter">01/10</span>
+                  <span class="section-next-label">Next chapter</span>
+                  <span class="section-next-icon"></span>
+                </a>
+              </div>
+            </section>
+            <section id="section-02" class="section animation">
+              <div class="section-body">
+                <div class="row">
+                  <div class="col col-xl-10">
+                    <h2
+                      class="section-title animation-translate-overline animation-item-1"
+                    >
+                      About me
+                    </h2>
+                    <article
+                      class="article animation-translate animation-item-2"
+                    >
+                      <p>
+                        Hello!<br />
+                        I'm a Full-Stack Web developer graduate with a stellar
+                        customer service and sales background. Eagerly seeking a
+                        Web Developer role at a company where I can utilize and
+                        further develop learned front end and back end
+                        technologies, namely; Ruby on Rails, JavaScript, HTML,
+                        CSS, SQL and Vue.js to create sophisticated and
+                        intuitive user experiences.
+                      </p>
+                    </article>
+                  </div>
+                </div>
+              </div>
+              <div class="section-footer animation-translate animation-item-3">
+                <a class="section-next goto-section" href="#section-03">
+                  <span class="section-next-counter">02/10</span>
+                  <span class="section-next-label">Next chapter</span>
+                  <span class="section-next-icon"></span>
+                </a>
+              </div>
+            </section>
+            <section id="section-08" class="section animation">
+              <div class="section-body">
+                <h2
+                  class="section-title animation-translate-overline animation-item-1"
+                >
+                  Projects
+                </h2>
+                <div class="row animation-translate animation-item-2">
+                  <div class="col-12 col-md-4">
+                    <a class="card" href="#modal-project-1" data-toggle="modal">
+                      <img
+                        class="card-img-top"
+                        src="https://via.placeholder.com/360x240"
+                        alt="Project 1"
+                      />
+                      <div class="card-body">
+                        <h3 class="card-title">Project 1</h3>
+                        <h4 class="card-subtitle">Design e-shop</h4>
+                        <p class="card-tags">Frontend — Backend</p>
+                      </div>
+                    </a>
+                  </div>
+                  <div class="col-12 col-md-4">
+                    <a class="card" href="#modal-project-2" data-toggle="modal">
+                      <img
+                        class="card-img-top"
+                        src="https://via.placeholder.com/360x240"
+                        alt="Project 2"
+                      />
+                      <div class="card-body">
+                        <h3 class="card-title">Project 2</h3>
+                        <h4 class="card-subtitle">
+                          Social Network for Musicians
+                        </h4>
+                        <p class="card-tags">Design — Frontend — Backend</p>
+                      </div>
+                    </a>
+                  </div>
+                  <div class="col-12 col-md-4">
+                    <a class="card" href="#modal-project-3" data-toggle="modal">
+                      <img
+                        class="card-img-top"
+                        src="https://via.placeholder.com/360x240"
+                        alt="Project 3"
+                      />
+                      <div class="card-body">
+                        <h3 class="card-title">Project 3</h3>
+                        <h4 class="card-subtitle">Jewelry e-shop</h4>
+                        <p class="card-tags">Frontend — Backend</p>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="section-footer animation-translate animation-item-3">
+                <a class="section-next goto-section" href="#section-09">
+                  <span class="section-next-counter">08/10</span>
+                  <span class="section-next-label">Next chapter</span>
+                  <span class="section-next-icon"></span>
+                </a>
+              </div>
+            </section>
+            <section id="section-10" class="section animation">
+              <div class="section-body">
+                <h2
+                  class="section-title animation-translate-overline animation-item-1"
+                >
+                  Contact
+                </h2>
+                <div class="row mb-10 animation-translate animation-item-2">
+                  <div class="col-12 col-md-4">
+                    <div class="contact">
+                      <strong class="contact-label">Stay in touch</strong>
+                      <a href="mailto:john@wilson.com">john@wilson.com</a><br />
+                      <a href="tel:+420652887351">+420 652 887 351</a>
+                    </div>
+                  </div>
+                  <div class="col-12 col-md-4">
+                    <div class="contact">
+                      <strong class="contact-label">Social</strong>
+                      <a href="https://www.facebook.com">Facebook</a><br />
+                      <a href="https://www.linked.com">LinekdIn</a><br />
+                      <a href="https://www.twitter.com">Twitter</a>
+                    </div>
+                  </div>
+                  <div class="col-12 col-md-4">
+                    <div class="contact">
+                      <strong class="contact-label">Address</strong>
+                      1234 Houston Street<br />
+                      New York<br />
+                      NY 123456<br />
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-12 col-xl-9">
+                    <h3
+                      class="section-subtitle animation-translate animation-item-3"
+                    >
+                      Leave a message
+                    </h3>
+                    <form
+                      class="needs-validation animation-translate animation-item-4"
+                      novalidate
+                    >
+                      <div class="row">
+                        <div class="col-12 col-md-6">
+                          <div class="form-group">
+                            <label for="contact-form-name">Name</label>
+                            <input
+                              type="text"
+                              class="form-control"
+                              name="name"
+                              id="contact-form-name"
+                              placeholder="Your name"
+                              required
+                            />
+                            <div class="invalid-feedback">
+                              Please enter your name.
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-12 col-md-6">
+                          <div class="form-group">
+                            <label for="contact-form-name">E-mail</label>
+                            <input
+                              type="email"
+                              class="form-control"
+                              name="email"
+                              id="contact-form-email"
+                              placeholder="@"
+                              required
+                            />
+                            <div class="invalid-feedback">
+                              Please enter a valid e-mail address.
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label for="contact-form-message">Message</label>
+                        <textarea
+                          class="form-control"
+                          name="message"
+                          id="contact-form-message"
+                          placeholder="Your message"
+                          rows="5"
+                          required
+                        ></textarea>
+                        <div class="invalid-feedback">
+                          Please type some message.
+                        </div>
+                      </div>
+                      <button type="submit" class="btn btn-primary">
+                        Send
+                      </button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+              <div class="section-footer animation-translate animation-item-5">
+                <span class="section-next goto-section">
+                  <span class="section-next-counter">10/10</span>
+                </span>
+              </div>
+            </section>
+          </div>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
